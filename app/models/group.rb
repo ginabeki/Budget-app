8b12d7c521002c72013ea5fd3f7683eaa9507dca
ginabeki <ginabeki3@gmail.com>
2023-03-23 15:24:28 +0300

    Add associations for group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
 class Group < ApplicationRecord
   has_many :users, through: :expenses
+  has_many :group_expenses, dependent: :destroy
   has_many :expenses, through: :group_expenses
-  has_many :group_expenses
+  validates :name, presence: true
+  validates :icon, presence: true
 end

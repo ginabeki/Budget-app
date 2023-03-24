d37bb67b379dab6e18705c018d934652bf81a681
ginabeki <ginabeki3@gmail.com>
2023-03-24 17:41:34 +0300

    Adjust association for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :expenses, foreign_key: 'author_id'
-  has_many :groups
+  has_many :groups, foreign_key: 'author_id'
 end
